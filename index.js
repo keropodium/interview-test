c5e2ab09880faf2206d1cdca8efd51010e8aa844
kutyel <flaviocorpa@gmail.com>
2017-07-22 12:04:20 +0200

    Add unit tests

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-import Emitter from './emitter';
-
-const emitter = new Emitter();
-emitter.emit('log', 0, 'should trigger nothing');
-
-const sub1 = emitter.subscribe('log', (...args) => console.log('sub1', ...args));
-emitter.emit('log', 1, 'should trigger 1 callback');
-// "sub1" 1 "should trigger 1 callback"
-
-const sub2 = emitter.subscribe('log', (...args) => console.log('sub2', ...args));
-emitter.emit('log', 2, 'should trigger 2 callbacks');
-// "sub1" 2 "should trigger 2 callback"
-// "sub2" 2 "should trigger 2 callback"
-
-sub1();
-emitter.emit('log', 3, 'should trigger 1 callback');
-// "sub2" 3 "should trigger 1 callback"
-
-const myEvent1 = emitter.subscribe('myEvent', () => 1);
-const myEvent2 = emitter.subscribe('myEvent', () => 2);
-const myEvent3 = emitter.subscribe('myEvent', () => true);
-const result = emitter.emit('myEvent');
-
-console.log(result);
-// [1, 2, true]
+export default class Emitter {
+    constructor() { }
+}
---
+++ test.js
@@ -0,0 +1,44 @@
+import expect from 'expect';
+
+import Emitter from '.';
+
+// Step 1
+
+let test = 0;
+
+const emitter = new Emitter();
+
+emitter.emit('sum', 1);
+
+expect(test).toBe(0); // should trigger nothing
+
+const sub1 = emitter.subscribe('sum', num => test = test + num);
+
+emitter.emit('sum', 1);
+
+expect(test).toBe(1); // should trigger 1 callback
+
+const sub2 = emitter.subscribe('sum', num => test = test * num);
+
+emitter.emit('sum', 2);
+
+expect(test).toBe(6); // should trigger 2 callbacks
+
+// Step 2
+
+sub1();
+
+emitter.emit('sum', 3);
+
+expect(test).toBe(18); // should trigger 1 callback
+
+// Step 3
+
+const myEvent1 = emitter.subscribe('myEvent', () => 1);
+const myEvent2 = emitter.subscribe('myEvent', () => 2);
+const myEvent3 = emitter.subscribe('myEvent', () => true);
+const result = emitter.emit('myEvent');
+
+expect(result).toEqual([1, 2, true]);
+
+console.info('You passed the test!!! 👏🏼 👏🏼 👏🏼')
